30fe944898a9b1e6061c480576b72eafc38614e2
vansh kalra <vanshkalra1379@gmail.com>
2024-08-18 21:12:03 +0530

    Added routing

--- Medium Backend/src/index.ts
+++ Medium Backend/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from 'hono'
-import { User } from '@prisma/client';
 import userRouter from './routes/userRouter';
 import blogRouter from './routes/blogRouter';
 
@@ -7,24 +6,5 @@ const app = new Hono();
 
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
-
-
-
-app.post('/api/v1/blog', async (c) => {
-  return c.text('Hello from blog post');
-});
-
-app.put('/api/v1/blog', async (c) => {
-  return c.text('Hello from blog post');
-});
-
-app.get('/api/v1/blog/:id', async (c) => {
-  const id = c.req.param('id');
-  return c.text(`Hello from blog post ${id}`);
-});
-
-app.get('/api/v1/blog/bulk', async (c) => {
-  return c.text('Hello from blog post bulk');
-});
 
 export default app
--- Medium Backend/src/routes/blogRouter.ts
+++ Medium Backend/src/routes/blogRouter.ts
@@ -1,5 +1,27 @@
 import { Hono } from "hono";
 
-const blogRouter = new Hono();
+const blogRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
+}>();
+
+blogRouter.post('/', async (c) => {
+    return c.text('Hello from blog post');
+});
+
+blogRouter.put('/', async (c) => {
+    return c.text('Hello from blog post');
+});
+
+blogRouter.get('/:id', async (c) => {
+    const id = c.req.param('id');
+    return c.text(`Hello from blog post ${id}`);
+});
+
+blogRouter.get('/bulk', async (c) => {
+    return c.text('Hello from blog post bulk');
+});
 
 export default blogRouter;

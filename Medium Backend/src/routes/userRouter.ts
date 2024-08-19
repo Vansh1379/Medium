1fb58e1582b0b58c8b2dd6da7a53d6e85d00d483
vansh kalra <vanshkalra1379@gmail.com>
2024-08-19 12:30:06 +0530

    Added zod in backend

--- Medium Backend/src/routes/userRouter.ts
+++ Medium Backend/src/routes/userRouter.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
+import { signupInput } from "../Zod/zod";
 
 export const userRouter = new  Hono<{
     Bindings: {
@@ -17,6 +18,14 @@ userRouter.post('/signup', async (c) => {
     }).$extends(withAccelerate())
 
     const body = await c.req.json();
+
+    const {success} = signupInput.safeParse(body);
+    if(!success){
+        c.status(411);
+        return c.json({
+            message: "Wrong Inputs Not validated by zod"
+        })
+    }
 
     try {
         const user = await prisma.user.create({
